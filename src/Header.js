15c2c472f434e396abcb523f292fc81b2e134e52
sarbada1 <sanjelsarbada12@gmail.com>
2023-12-13 15:34:29 +0545

    react components

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar,Nav,Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+
+function Header() {
+  return (
+    <div>
+
+<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">E-com</Navbar.Brand>
+          <Nav className="me-auto nav-wrap">
+            <Link to="/add">Add product</Link>
+            <Link to="/update">Update product</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          
+          </Nav>
+        </Container>
+      </Navbar>
+      
+
+    </div>
+  )
+}
+
+export default Header
